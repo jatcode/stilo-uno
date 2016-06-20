c6d64d149f6994c493d1b8ac59570bc22c07d09e
dev master <devmaster2015@gmail.com>
2016-06-19 21:42:31 -0600

    first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#What's in here 
+So this project is a test about unifiying angular2 and webpack from scratch
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:"my-app",
+  template: '<h1>Creo q cmaiareambiio</h1>'
+})
+
+export class AppComponent {
+
+}
